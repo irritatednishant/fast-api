a23f9793e29a5e8d7727bf0d9ad3d1fb0628bcae
nishant <irritatednishant@gmail.com>
2025-12-02 17:46:52 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,57 @@
+from fastapi import FastAPI, Path, HTTPException, Query
+import json
+from pydantic import BaseModel ,Field, computed_field
+from typing import Annotated, Literal
+app = FastAPI()
+
+class Student(BaseModel):
+    id:Annotated[str, Field(..., description='ID of the student')]
+    name:Annotated[str,Field(...,description='Name of the student')]
+    course:Annotated[str,Field(...,description='Course in which student is enrolled')]
+    enrollment_no:Annotated[int,Field(...,gt=0,description='Enrollment no of the student')]
+    address:Annotated[str,Field(...,description='Address of the student.')]
+    gender:Annotated[Literal['Male','Female'],Field(...,description='Gender of the student')]
+    cgpa:Annotated[float,Field(...,gt=0,le=10,description='CGPA of the student')]
+
+    pass
+
+def load_data():
+    with open('student.json','r') as f:
+        data = json.load(f) 
+    return data
+
+@app.get("/")
+def hello():
+    return {"message":"hello world"}
+
+@app.get("/view")
+def view():
+    data = load_data()
+    return data
+
+@app.get("/student/{student_id}")
+def vies_student(student_id:str =Path(...,description="ID of student in db", example='[1,2,3]')):
+    data = load_data()
+    student = next((s for s in data if s['id'] == int(student_id) ),None)
+    if student:
+        return student
+    raise HTTPException(status_code=404, detail="Student not found!")
+
+
+@app.get("/sort")
+def sort_student(sort_by: str = Query(...,description='Sort student pn the basis of their name, enrolment and cgpa'),
+                  order: str = Query('asc',description='Sort in asc or desc')):
+    valid_fields = ['name','enrolment_no','cgpa']
+    if sort_by not in valid_fields:
+        raise HTTPException(status_code=400,detail=f'Invalid field select from {valid_fields}')
+    
+    if order not in ['asc','desc']:
+        raise HTTPException(status_code=400,detail='Invalid order select between asc and desc')
+    
+    data = load_data()
+    sort_order = True if order=='desc' else False
+
+    sorted_data = sorted(data, key = lambda x: x[sort_by],reverse=sort_order)
+
+    return sorted_data
+    
