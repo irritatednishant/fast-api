6dd8fdd9942a1472e81c59c32c2213e1096e2d02
nishant <irritatednishant@gmail.com>
2025-12-02 19:33:44 +0530

    create endpoint created

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI, Path, HTTPException, Query
 import json
+from fastapi.responses import JSONResponse
 from pydantic import BaseModel ,Field, computed_field
 from typing import Annotated, Literal
 app = FastAPI()
 
 class Student(BaseModel):
-    id:Annotated[str, Field(..., description='ID of the student')]
+    id:Annotated[int, Field(...,description='ID of the student',)]
     name:Annotated[str,Field(...,description='Name of the student')]
     course:Annotated[str,Field(...,description='Course in which student is enrolled')]
     enrollment_no:Annotated[int,Field(...,gt=0,description='Enrollment no of the student')]
@@ -20,6 +21,12 @@ def load_data():
         data = json.load(f) 
     return data
 
+
+def save_obj(data):
+    with open('student.json','w') as f:
+        json.dump(data,f)
+
+
 @app.get("/")
 def hello():
     return {"message":"hello world"}
@@ -27,10 +34,11 @@ def hello():
 @app.get("/view")
 def view():
     data = load_data()
+    print(data)
     return data
 
 @app.get("/student/{student_id}")
-def vies_student(student_id:str =Path(...,description="ID of student in db", example='[1,2,3]')):
+def view_student(student_id:str =Path(...,description="ID of student in db",examples='[1,2,3]')):
     data = load_data()
     student = next((s for s in data if s['id'] == int(student_id) ),None)
     if student:
@@ -41,7 +49,7 @@ def vies_student(student_id:str =Path(...,description="ID of student in db", exa
 @app.get("/sort")
 def sort_student(sort_by: str = Query(...,description='Sort student pn the basis of their name, enrolment and cgpa'),
                   order: str = Query('asc',description='Sort in asc or desc')):
-    valid_fields = ['name','enrolment_no','cgpa']
+    valid_fields = ['name','enrollment_no','cgpa']
     if sort_by not in valid_fields:
         raise HTTPException(status_code=400,detail=f'Invalid field select from {valid_fields}')
     
@@ -55,3 +63,21 @@ def sort_student(sort_by: str = Query(...,description='Sort student pn the basis
 
     return sorted_data
     
+@app.post('/create')
+def create(student:Student):
+
+    data = load_data()
+
+    if any(int(student.id) == obj['id'] for obj in data):
+        raise HTTPException(status_code=400,detail='Student already exists!')
+    
+    data.append(student.model_dump())
+
+    save_obj(data)
+
+    return JSONResponse(status_code=201,content={'message':'Student details added sucessfully!'})
+
+
+if "__name__" == "__main__":
+    data = load_data()
+    print(data)
